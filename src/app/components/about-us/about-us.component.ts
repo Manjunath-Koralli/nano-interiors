b9710d5acb522a286371c4e6cd39b95a1f65df85
Manju <mskoralli@gmail.com>
2021-08-04 00:03:22 +0530

    about us completed

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { faGlobeAsia, faHeadphonesAlt, faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft, faStar as faStar1, faMapMarkerAlt, faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStar2 } from '@fortawesome/free-regular-svg-icons';
+import { faInstagram, faInstagramSquare, faWhatsapp, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons';
+import { faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import * as $ from 'jquery';
+import { MapsAPILoader } from '@agm/core';
 
 @Component({
   selector: 'app-about-us',
@@ -7,9 +14,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
-  constructor() { }
+  faGlobeAsia = faGlobeAsia;
+  faHeadphonesAlt = faHeadphonesAlt;
+  faEnvelopeOpenText = faEnvelopeOpenText;
+
+  faQuoteLeft = faQuoteLeft;
+  faStar1 = faStar1;
+  faStar2 = faStar2;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faEnvelope = faEnvelope;
+  faPhoneAlt = faPhoneAlt;
+
+  faInstagram = faInstagram;
+  faInstagramSquare = faInstagramSquare;
+  faPhoneSquareAlt = faPhoneSquareAlt;
+  faWhatsapp = faWhatsapp;
+  faWhatsappSquare = faWhatsappSquare;
+
+  latitude!: number;
+  longitude!: number;
+  zoom!: number;
+  //address!: string;
+  private geoCoder: any;
+  constructor(private mapsAPILoader: MapsAPILoader,private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    $('.counter').each(function () {
+      $(this)
+        .prop('Counter', 0)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+          }
+        );
+    });
+
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+    });
+  
+  }
+
+  setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        console.log(this.latitude,this.longitude)
+        //this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+ 
+  markerDragEnd($event: any) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    //this.getAddress(this.latitude, this.longitude);
   }
 
 }
