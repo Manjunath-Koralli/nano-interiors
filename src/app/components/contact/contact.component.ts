c6b4c109390cba2e751e2b098e6d96749f4418ec
Manju <mskoralli@gmail.com>
2021-08-06 00:34:15 +0530

    contact us page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { faGlobeAsia, faHeadphonesAlt, faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft, faStar as faStar1, faMapMarkerAlt, faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStar2 } from '@fortawesome/free-regular-svg-icons';
+import { faInstagram, faInstagramSquare, faWhatsapp, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons';
+import { faPhoneSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { MapsAPILoader } from '@agm/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  faGlobeAsia = faGlobeAsia;
+  faHeadphonesAlt = faHeadphonesAlt;
+  faEnvelopeOpenText = faEnvelopeOpenText;
+
+  faQuoteLeft = faQuoteLeft;
+  faStar1 = faStar1;
+  faStar2 = faStar2;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faEnvelope = faEnvelope;
+  faPhoneAlt = faPhoneAlt;
+
+  faInstagram = faInstagram;
+  faInstagramSquare = faInstagramSquare;
+  faPhoneSquareAlt = faPhoneSquareAlt;
+  faWhatsapp = faWhatsapp;
+  faWhatsappSquare = faWhatsappSquare;
+
+  latitude!: number;
+  longitude!: number;
+  zoom!: number;
+  //address!: string;
+  private geoCoder: any;
+  constructor(private mapsAPILoader: MapsAPILoader,private ngZone: NgZone) { }
 
   ngOnInit(): void {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+    });
+  }
+
+  setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        console.log(this.latitude,this.longitude)
+        //this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+ 
+  markerDragEnd($event: any) {
+    console.log($event);
+    this.latitude = $event.coords.lat;
+    this.longitude = $event.coords.lng;
+    //this.getAddress(this.latitude, this.longitude);
   }
 
 }
