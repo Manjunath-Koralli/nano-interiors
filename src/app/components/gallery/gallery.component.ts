c5dddea2562222a9849edf49c5b95312a74cbdab
Manju <mskoralli@gmail.com>
2021-08-17 19:13:44 +0530

    premium projects

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -10,9 +10,18 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   images = [
-    "assets/images/proj-1.jpg",
-    "assets/images/proj-2jpg",
-    "assets/images/proj-3.jpg"
+    "assets/images/final-images/premium/jayanti/j1.JPG",
+    "assets/images/final-images/premium/jayanti/j2.jpg",
+    "assets/images/final-images/premium/jayanti/j3.jpg",
+    "assets/images/final-images/premium/jayanti/j4.jpg",
+    "assets/images/final-images/premium/jayanti/j5.jpg",
+    "assets/images/final-images/premium/jayanti/j6.jpg",
+    "assets/images/final-images/premium/jayanti/j7.jpg",
+    "assets/images/final-images/premium/jayanti/j8.jpg",
+    "assets/images/final-images/premium/jayanti/j9.JPG",
+    "assets/images/final-images/premium/jayanti/j10.JPG",
+    "assets/images/final-images/premium/jayanti/j11.JPG",
+    "assets/images/final-images/premium/jayanti/j12.JPG",
   ]
   ngOnInit(): void {
   }
